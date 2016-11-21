4ae94e421738ec71710530e67428594e37b9def1
dzmitryh <dzmitry.hubin@gmail.com>
2016-11-21 22:57:04 +0300

    Lab4-initial version has been committed.

---
+++ lab4-subject/src/main/java/by/spring/eureka/SubjectClientApplication.java
@@ -0,0 +1,14 @@
+package by.spring.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SubjectClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubjectClientApplication.class, args);
+	}
+}
