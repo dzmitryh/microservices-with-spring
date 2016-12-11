b648b41732f8b21880181a26e6d659e1067b448f
dzmitryh <dzmitry.hubin@gmail.com>
2016-12-11 20:15:44 +0300

    changed name of the app.

--- lab4/lab4-eureka-client-solution/run-all.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-mvn spring-boot:run -Dspring.profiles.active=subject &
-mvn spring-boot:run -Dspring.profiles.active=verb &
-mvn spring-boot:run -Dspring.profiles.active=article &
-mvn spring-boot:run -Dspring.profiles.active=adjective &
-mvn spring-boot:run -Dspring.profiles.active=noun &
-mvn spring-boot:run -Dspring.profiles.active=sentence
